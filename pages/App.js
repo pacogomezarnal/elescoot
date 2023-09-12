8c3a1540f738591ddd6a31c9e66060bad12804c7
Paco Mastermind <paco@mastermind.ac>
2023-09-12 13:19:20 +0200

    Contexto

--- pages/App.js
+++ pages/App.js
@@ -1,14 +1,22 @@
 import Header from "./layout/Header"
 import ScooterList from "./scooter/ScooterList";
 import Scooter from "./scooter/Scooter";
-import { useState,useEffect } from 'react';
+import { useState,useEffect,createContext } from 'react';
+
+export const ThemeContext = createContext({})
 
 const App = ()=> {
     const [selectedSc,setSelectedSc] = useState()
+    const [darkTheme, setDarkTheme] = useState(false);
+
+    const cambiarDarkTheme = () => setDarkTheme(!darkTheme);
+
     return(
         <>
-            <Header/>
-            {selectedSc?<Scooter scooter={selectedSc} />:<ScooterList selectedSc={setSelectedSc}/>}
+            <ThemeContext.Provider value={{darkTheme,cambiarDarkTheme}}>
+                <Header/>
+                {selectedSc?<Scooter scooter={selectedSc} />:<ScooterList selectedSc={setSelectedSc}/>}
+            </ThemeContext.Provider>
         </>
     )
 }
--- pages/layout/Header.js
+++ pages/layout/Header.js
@@ -1,5 +1,8 @@
+import { useContext } from "react";
+import { ThemeContext } from "../App";
 
 const Header = () => {
+    const {darkTheme,cambiarDarkTheme} = useContext(ThemeContext);
     return(
         <nav className="navbar fixed-top bg-body-secondary">
             <div className="container-fluid">
@@ -7,6 +10,14 @@ const Header = () => {
                     <img src="./img/logo.png" alt="Logo" height="35" class="d-inline-block align-text-top"/>
                     <span className="fs-2 ps-3 fw-bold">ElecScoot</span>
                 </a>
+                <div className="form-check form-switch">
+                    <input class="form-check-input" 
+                    type="checkbox" role="switch" 
+                    id="flexSwitchCheckDefault" 
+                    checked={darkTheme===true}
+                    onChange={()=>{cambiarDarkTheme();}}/>
+                    <label className={darkTheme?"form-check-label text-light":"form-check-label"} for="flexSwitchCheckDefault">Oscuro</label>
+                </div>
             </div>
         </nav>
     )
